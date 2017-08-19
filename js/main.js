995106bbd5d22d6172396513d264f4228b0774a8
ashrafishaheen <ashrafi.shaikh96@gmail.com>
2017-08-20 01:59:49 +0530

    fetching movies by angular

--- js/main.js
+++ js/main.js
@@ -1,48 +1,40 @@
-$(document).ready( function() {
-$('#searchForm').on('submit',function(e){
-  var searchText = $('#searchText').val();
-  getMovies(searchText);
-  e.preventDefault();
+var app = angular.module('myApp', ["ngRoute"]);
+app.factory('GetData', function($http){
+  return{
+    movieData: function(type,passdata, response){
+      var urlLink = "http://api.cinemalytics.in/v2/movie/" + type + "/" + passdata + "/?auth_token=6DB9B962A69E74C73133EC431A6B05D8";
+      $http.get(urlLink).success(response);
+    }
+  }
 });
+app.controller('allMovie',function($scope, GetData, $routeParams, $route, $location){
+$scope.year = $routeParams.year;
 
+GetData.movieData('year',$routeParams.year,function(response){
+$scope.movie = response;
 });
-function getMovies(searchText){
-
-
-/*axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=cedb5e5c'+searchText)
-.then (function(response) {
-  console.log(response);
-})
-.catch (function(err) {
-  console.log(err);
-}); */
-
-// Make a request for a user with a given ID
-
-
-axios.get('https://api.themoviedb.org/3/movie/550?api_key=d67ad8b8ff97775c9ec20d39eb1df389')
-  .then(function (response) {
-    console.log(response);
-    var movies = response.data.genres;
-    var output = '';
-    $.each(genres,(index, genres) => {
-      output += `<div class="col-md-3">
-      <div class="well text-center">
-      <img src="${genres.poster_path}">
-      <h5>${genres.title}</h5>
-      <a onclick="movieSelected('${genres.imdb_id}')"class="btn btn-primary" href="#">Movies Details</a>
-      </div>
-      </div>
-
-      `;
-    });
-
-
-    $('#movie').html(output);
+$scope.gotoMovie = function(id){
+  $location.path("/id/" + id);
+};
+//redirect to page
+$scope.gotoMovieyear =function(year){
+  $location.path("/year/" + year);
+};
+});
+app.controller('singleMovie',function($scope, GetData, $routeParams){
+GetData.movieData('id', $routeParams.ID,function(response){
+  $scope.movie = response;
+});
+});
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/year/:year", {
+        templateUrl : "templets/main.html",
+        controller: "allMovie"
+    })
+    .when("/id/:ID", {
+        templateUrl : "templets/single.html",
+        controller: "singleMovie"
   })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-  //console.log(searchText);
-}
+  .otherwise({redirectTo: '/year/2017'});
+});
